257296a7af12a5c8853a03782b1392fc220e075a
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2022-12-25 22:49:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sklearn
+import sklearn as sk
 import warnings
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
